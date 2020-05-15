d2bcb227907983e609814bb221b87537f872f5cf
Dhruval javia <46935683+dhruvaljavia@users.noreply.github.com>
2020-05-15 23:20:58 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-print('Hello,what is your name?')
-name = input()
-print('Hi ' + name + '!')
-print('Enter any number')
-a = input()
-if int(a)%2==0 :
-    print('You entered even no.!')
-else :
-    print('You entered odd no.!')
