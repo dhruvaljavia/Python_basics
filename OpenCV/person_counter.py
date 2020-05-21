7a1e4ca5779d79e328b4e011af8c3d30312a8f66
Dhruval javia <46935683+dhruvaljavia@users.noreply.github.com>
2020-05-21 16:54:54 +0530

    Added periodic cross line updation and comments

--- OpenCV/person_counter.py
+++ OpenCV/person_counter.py
@@ -4,11 +4,14 @@ import numpy as np
 cap = cv2.VideoCapture(700)
 # CAP_PROP_FRAME_HEIGHT = 480
 # CAP_PROP_FRAME_WIDTH = 640
-ctr = 1
-thresh = 200
+
+ctr = 0 # Counter for updating the initial cross lines
+thresh = 200 # For distinguishing between pixel noise and crossing person
+
 cL=cR=cLX=cRX=0
 flgL=flgR=0
 
+# Sets the initial cross lines
 cv2.waitKey(2000)
 _,init = cap.read()
 B,G,R = cv2.split(init)
@@ -16,19 +19,24 @@ Rline = np.int32(B[0:480,180:190])
 Lline = np.int32(B[0:480,450:460])
 cv2.waitKey(2000)
 
+# Reads and displays the web cam feed
 while 1:
+	ctr = ctr + 1
 	_,frame = cap.read()
 	
+	# Extracts current cross lines from the frame
 	b,g,r = cv2.split(frame)
 	cRline = np.int32(b[0:480,180:190])
 	cLline = np.int32(b[0:480,450:460])
 
+	# Calculates absolute difference between current and initial cross lines
 	diffR = np.abs(cRline - Rline)
 	diffL = np.abs(cLline - Lline)
 
+	# IF-ELSE contruct which determines if a person has crossed from left or right
 	if np.any(diffR>thresh): 
 		if cL==0 and flgR==0:
-			print(str(ctr)+' >>crossed from right!')
+			print('crossed from right!')
 			cR=1
 			flgR=1
 		elif cL==1:
@@ -42,7 +50,7 @@ while 1:
 
 	if np.any(diffL>thresh):
 		if cR==0 and flgL==0:
-			print(str(ctr)+' >>crossed from left!')	
+			print('crossed from left!')	
 			cL=1
 			flgL=1
 		elif cR==1:
@@ -54,17 +62,23 @@ while 1:
 		flgR = flgL = 0
 		
 
+	# Displays the web cam feed
 	cv2.rectangle(frame,(180,480),(190,0),(0,0,0),-1)
 	cv2.rectangle(frame,(450,480),(460,0),(0,0,0),-1)
 	fframe = np.flip(frame,1)
-
 	cv2.imshow('webcam_feed',fframe)
-	cv2.imshow('gray',np.flip(b,1))
+	#cv2.imshow('gray',np.flip(b,1))
+
 
 	if cv2.waitKey(1) == ord('q'):
 		break
 
-	ctr = ctr + 1
+
+	# Updates the initial cross lines after every 500 iterations(< 1 min)
+	if ctr%500==0:
+		Rline = np.int32(b[0:480,180:190])
+		Lline = np.int32(b[0:480,450:460])
+		ctr=0
 
 cap.release()
 cv2.destroyAllWindows()
